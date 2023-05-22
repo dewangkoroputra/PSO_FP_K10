6d433e93408794996b4177f9550e028b5ba71d08
naufalfano <naufal.fano@gmail.com>
2023-05-22 12:12:31 +0700

    Update tailwind.config.js
    design style#1

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,8 +12,44 @@ export default {
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                'poppins': ['Poppins', 'ui-sans-serif']
+                //sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
+
+            fontSize: {
+                xs: ['10px', '16px'],
+                sm: ['12px', '16px'],
+                base: ['14px', '24px'],
+                lg: ['16px', '24px'],
+                xl: ['18px', '24px'],
+                '2xl': ['20px', '32px'],
+                '3xl': ['24px', '32px'],
+                '4xl': ['28px', '40px'],
+                '5xl': ['30px', '40px'],
+                '6xl': ['32px', '48px'],
+                '7xl': ['40px', '56px']
+            },
+
+            colors: {
+                //Neutral
+                'neutral-10': '#FFFFFF',
+                'neutral-20': '#DCDCDC',
+                'neutral-30': '#D5D5D5',
+                'neutral-40': '#C9C9C9',
+                'neutral-50': '#AEAEAE',
+                'neutral-60': '#8E8E8E',
+                'neutral-70': '#6E6E6E',
+                'neutral-80': '#393939',
+                'neutral-90': '#DCDCDC',
+
+                //Primary
+                'primary-main': '#045BB2',
+                'primary-surface': '#EFF5FA',
+                'primary-border': '#B1CCE7',
+                'primary-hover': '#034C94',
+                'primary-pressed': '#022E59',
+                'primary-focused': '#B8C9DB',
+            }
         },
     },
 
