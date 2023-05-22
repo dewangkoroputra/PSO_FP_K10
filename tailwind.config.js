533c9cd23676b857d7822d15a3a4d88ee6da7fbd
naufalfano <naufal.fano@gmail.com>
2023-05-22 22:04:29 +0700

    Update tailwind.config.js
    design-style

--- tailwind.config.js
+++ tailwind.config.js
@@ -49,6 +49,66 @@ export default {
                 'primary-hover': '#034C94',
                 'primary-pressed': '#022E59',
                 'primary-focused': '#B8C9DB',
+
+                //Grey
+                'grey-50': '#F9FAFB',
+                'grey-100': '#F3F4F6',
+                'grey-200': '#E5E7EB',
+                'grey-300': '#D1D5DB',
+                'grey-400': '#9CA3AF',
+                'grey-500': '#6B7280',
+                'grey-600': '#4B5563',
+                'grey-700': '#374151',
+                'grey-800': '#1F2937',
+                'grey-900': '#111827',
+
+                //Blue
+                'blue-50': '#EBF5FF',
+                'blue-100': '#E1EFFE',
+                'blue-200': '#C3DDFD',
+                'blue-300': '#A4CAFE',
+                'blue-400': '#76A9FA',
+                'blue-500': '#3F83F8',
+                'blue-600': '#1C64F2',
+                'blue-700': '#1A56DB',
+                'blue-800': '#1E429F',
+                'blue-900': '#233876',
+
+                //Red
+                'red-50': '#FDF2F2',
+                'red-100': '#FDE8E8',
+                'red-200': '#FBD5D5',
+                'red-300': '#F8B4B4',
+                'red-400': '#F98080',
+                'red-500': '#F05252',
+                'red-600': '#E02424',
+                'red-700': '#C81E1E',
+                'red-800': '#9B1C1C',
+                'red-900': '#771D1D',
+
+                 //Green
+                'green-50': '#F3FAF7',
+                'green-100': '#DEF7EC',
+                'green-200': '#BCF0DA',
+                'green-300': '#84E1BC',
+                'green-400': '#31C48D',
+                'green-500': '#0E9F6E',
+                'green-600': '#057A55',
+                'green-700': '#046C4E',
+                'green-800': '#03543F',
+                'green-900': '#014737',
+
+                 //Yellow
+                'yellow-50': '#FDFDEA',
+                'yellow-100': '#FDF6B2',
+                'yellow-200': '#FCE96A',
+                'yellow-300': '#FACA15',
+                'yellow-400': '#E3A008',
+                'yellow-500': '#C27803',
+                'yellow-600': '#9F580A',
+                'yellow-700': '#8E4B10',
+                'yellow-800': '#723B13',
+                'yellow-900': '#633112'
             }
         },
     },
