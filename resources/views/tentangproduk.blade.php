0e836b2e76faf33ec82d8f8dc8cdb996b0fd7fee
EvanrizaSA <ariadi.evanriza@gmail.com>
2023-05-31 16:24:29 +0700

    tentangproduk

---
+++ resources/views/tentangproduk.blade.php
@@ -0,0 +1,46 @@
+@extends('layout')
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    @vite('resources/css/app.css')
+
+    <title>@yield('RanmITS')</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.js"></script>
+
+</head>
+@section('content')
+<body class="px-6 max-w-xl mx-auto bg-slate-100">
+    <div class="inline-flex flex-col space-y-3 items-start justify-start w-80">
+        <div class="inline-flex space-x-2.5 items-center justify-end w-28 h-1/2">
+            <img src="/assets/vertical-logo-text.png">
+        </div>
+    <p class="text-xs font-medium leading-none text-gray-700">Platform pencarian kendaraan hilang</p>
+    </div>
+    <div class="flex flex-col space-y-4 items-start justify-center w-80">
+        <div class="inline-flex space-x-4 items-center justify-start w-full">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z" />
+            </svg>
+        <p class="text-lg font-semibold leading-relaxed text-gray-700">Tentang Produk</p>
+        </div>
+        <p class="w-full text-xs leading-none text-gray-500">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    </div>
+    <div class="inline-flex flex-col space-y-4 items-start justify-center w-80">
+        <div class="inline-flex space-x-4 items-center justify-start w-full">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z" />
+            </svg>
+            <p class="text-lg font-semibold leading-relaxed text-gray-700">Tentang Pengembang</p>
+        </div>
+    <p class="w-full text-xs leading-none text-gray-500">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    </div>
+</body>
+@endsection
