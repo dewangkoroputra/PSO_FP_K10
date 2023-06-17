aec3193ceaa64c0b106f93ee0abea884c0f273b0
EvanrizaSA <ariadi.evanriza@gmail.com>
2023-06-17 22:05:38 +0700

    fix : placeholder

--- resources/views/formlaporan.blade.php
+++ resources/views/formlaporan.blade.php
@@ -179,7 +179,7 @@
                     <label for="lokasi_kejadian"
                         class="block mb-2 text-sm font-poppins font-medium text-gray-900 dark:text-white">Lokasi
                         Kejadian</label>
-                    <input placeholder="Masukkan model kendaraan" required type="text" id="lokasi_kejadian"
+                    <input placeholder="Masukkan lokasi kejadian" required type="text" id="lokasi_kejadian"
                         name="lokasi_kejadian"
                         class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                 </div>
